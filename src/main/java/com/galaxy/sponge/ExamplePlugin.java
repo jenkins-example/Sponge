31c15479450509c10422379b40ec53455baf1a70
趙子賢 <danny900714@gmail.com>
2019-08-11 23:26:22 +0800

    Add plugin.yml and plugin main file

---
+++ src/main/java/com/galaxy/sponge/ExamplePlugin.java
@@ -0,0 +1,19 @@
+package com.galaxy.sponge;
+
+import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(id="spongejenkins")
+public class ExamplePlugin {
+
+    @Inject
+    private Logger logger;
+
+    @Listener
+    public void onServerStart(GameStartedServerEvent event) {
+        logger.info("Server start!");
+    }
+}
